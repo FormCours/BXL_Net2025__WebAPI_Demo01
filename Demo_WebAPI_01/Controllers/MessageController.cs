536cd62178020dc70c94c8e6101ef71c8ecb4671
Aurelien St <aurelien.strimelle@bstorm.be>
2025-12-08 15:55:14 +0100

    Controller Message

---
+++ Demo_WebAPI_01/Controllers/MessageController.cs
@@ -0,0 +1,38 @@
+﻿using Demo_WebAPI_01.Dto;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Demo_WebAPI_01.Controllers
+{
+    [Route("api/[controller]")]  // → Il utilise "Message" comme nom de route, car c'est le nom du Controller
+    [ApiController]
+    public class MessageController : ControllerBase
+    {
+        // (GET) /api/message
+        [HttpGet]   
+        public IActionResult MessageOfDay()
+        {
+            // Traitement...
+            string msg = $"Bonjour, nous sommes le {DateTime.Today.ToShortDateString()}";
+
+            // Réponse du endpoint
+            // - Renvoyer le status http adapté (Ok - 200)
+            // - Donnée formatée avec le Dto désiré
+            return Ok(new MessageResponseDto()
+            {
+                Message = msg
+            });
+        }
+
+        // (GET) /api/message/hello
+        [HttpGet("hello")]   
+        public IActionResult SayHello()
+        {
+            return Ok(new MessageResponseDto()
+            {
+                Message = "Hello World"
+            });
+        }
+
+    }
+}
---
+++ Demo_WebAPI_01/Dto/MessageDto.cs
@@ -0,0 +1,7 @@
+﻿namespace Demo_WebAPI_01.Dto
+{
+    public class MessageResponseDto
+    {
+        public required string Message { get; init; }
+    }
+}
