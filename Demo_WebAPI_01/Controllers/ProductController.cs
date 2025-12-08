2bb8fe68ef426c099a48e09a2ad3214286c56f5f
Aurelien St <aurelien.strimelle@bstorm.be>
2025-12-08 16:05:06 +0100

    Mini exo non obligatoire

---
+++ Demo_WebAPI_01/Controllers/ProductController.cs
@@ -0,0 +1,18 @@
+﻿using Demo_WebAPI_01.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Demo_WebAPI_01.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        // Remarque, L'utilisation du service sera amélioré mercredi :p
+        private ProductService _productService = new ProductService();
+
+        // TODO Faire la méthode pour envoyer la liste des produits
+
+        // TODO Faire la méthode pour envoyer  un produit via son id
+    }
+}
