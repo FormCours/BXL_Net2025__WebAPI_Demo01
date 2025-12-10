8e55f87d438955a09405ce03784db39bf35499f7
Aurélien St <aurelien.strimelle@bstorm.be>
2025-12-10 11:46:21 +0100

    Endpoints Product GET

--- Demo_WebAPI_01/Controllers/ProductController.cs
+++ Demo_WebAPI_01/Controllers/ProductController.cs
@@ -1,9 +1,19 @@
-﻿using Demo_WebAPI_01.Services;
+﻿using Demo_WebAPI_01.Dto;
+using Demo_WebAPI_01.Models;
+using Demo_WebAPI_01.Services;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Demo_WebAPI_01.Controllers
 {
+    // Role du controller :
+    //  - Exposer les routes (endpoints) via des méthodes
+
+    // Role des méthodes du controller : 
+    //  - Interpréter les données de la requete (route, parametre, body, ...)
+    //  - Réaliser le traitement par l'intermedaire du service
+    //  - Envoyer la réponse adapté (Status de la requete & les données au format DTO)
+
     [Route("api/[controller]")]
     [ApiController]
     public class ProductController : ControllerBase
@@ -11,8 +21,40 @@ namespace Demo_WebAPI_01.Controllers
         // Remarque, L'utilisation du service sera amélioré mercredi :p
         private ProductService _productService = new ProductService();
 
-        // TODO Faire la méthode pour envoyer la liste des produits
+        // (GET) /api/product
+        // Faire la méthode pour envoyer la liste des produits
+        [HttpGet]
+        public IActionResult GetAllProducts()
+        {
+            IEnumerable<Product> products = _productService.GetAll();
 
-        // TODO Faire la méthode pour envoyer  un produit via son id
+            return Ok(products.Select(p => new ProductItemResponseDto()
+            {
+                Id = p.Id,
+                Name = p.Name,
+            }));
+        }
+
+        // (GET) /api/product/{id}
+        // Faire la méthode pour envoyer un produit via son id
+        [HttpGet("{id}")]
+        public IActionResult GetProductById(int id)
+        {
+            Product? product = _productService.GetById(id);
+            if (product is null) 
+            {
+                return NotFound("Erreur : pas de produit trouvé");
+            }
+            
+            return Ok(new ProductDetailResponseDto()
+            {
+                Id = product.Id,
+                Name = product.Name,
+                Description = product.Description ?? "N/A",
+                Price = product.Price,
+                InStock = product.InStock
+            });
+        }
+
     }
 }
---
+++ Demo_WebAPI_01/Dto/ProductDto.cs
@@ -0,0 +1,17 @@
+﻿namespace Demo_WebAPI_01.Dto
+{
+    public class ProductItemResponseDto
+    {
+        public required int Id { get; set; }
+        public required string Name { get; set; }
+    }
+
+    public class ProductDetailResponseDto
+    {
+        public required int Id { get; set; }
+        public required string Name { get; set; }
+        public required string Description { get; set; }
+        public required double Price { get; set; }
+        public required bool InStock { get; set; }
+    }
+}
